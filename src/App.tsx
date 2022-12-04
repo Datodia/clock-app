1e1cd91a990783a142593dcf773e4fccf45669c9
Datodia <dato.diasamidze.02@gmail.com>
2022-12-04 17:47:48 +0400

    init: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import styled from 'styled-components'
+
+function App() {
+
+  return (
+    <Wrapper>
+      <h1>hello</h1>
+    </Wrapper>
+  )
+}
+
+export default App
+
+const Wrapper = styled.div`
+  background-image: url('assets/mobile/bg-image-daytime.jpg');
+  height: 100vh;
+`
+
+
